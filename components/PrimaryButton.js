6552d3d02e4bd94010441dd9ca42f029548db60e
StocktonManges <stocktonmanges@gmail.com>
2024-03-14 16:08:04 -0600

    Added button styling and number input styling.

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,26 +1,44 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 
 export default function PrimaryButton({ children }) {
+  const pressHandler = () => {};
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? // If pressed, use default buttonInnerContainer styles AND the pressed styles.
+              [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    width: 100,
-    height: "fit-content",
-    borderRadius: 8,
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
     overflow: "hidden",
-    margin: 5,
   },
-  button: {
-    height: "fit-content",
-    backgroundColor: "cyan",
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
     paddingVertical: 8,
     paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
     textAlign: "center",
+  },
+  // This is to show feedback on iOS after the button is pressed since
+  // the "android_ripple" prop only works on androids.
+  pressed: {
+    opacity: 0.75,
   },
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,12 @@ import PrimaryButton from "../components/PrimaryButton";
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        // Some keyboard types are not compatible across platforms.
+        keyboardType="number-pad"
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -16,7 +21,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#27063c",
+    backgroundColor: "#4e0329",
     borderRadius: 8,
     // BOX SHADOW
     elevation: 4, // Android
@@ -24,5 +29,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 }, // iOS
     shadowRadius: 6, // iOS
     shadowOpacity: 0.5, // iOS
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
