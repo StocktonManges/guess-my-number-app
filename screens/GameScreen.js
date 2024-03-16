942de2a8d00bdca3ec3fdf7273f3f5229849c7fd
StocktonManges <stocktonmanges@gmail.com>
2024-03-16 14:45:47 -0600

    Added number guessing and checking logic and game over screen.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,13 +1,73 @@
-import { View, Text, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import { View, Text, StyleSheet, Alert } from "react-native";
+import Title from "../components/ui/Title";
+import { useEffect, useState } from "react";
+import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
-export default function GameScreen() {
+const generateRandomBetween = (min, max, exclude) => {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  }
+  return rndNum;
+};
+
+let min = 1;
+let max = 100;
+
+export default function GameScreen({ userNumber, onGameOver }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  useEffect(() => {
+    if (currentGuess === userNumber) {
+      onGameOver();
+    }
+  }, [currentGuess, userNumber, onGameOver]);
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < userNumber) ||
+      (direction === "higher" && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        { text: "I'm sorry", style: "cancel" },
+      ]);
+      return;
+    }
+
+    if (direction === "lower") {
+      max = currentGuess - 1;
+    } else {
+      min = currentGuess + 1;
+    }
+    const newRndNumber = generateRandomBetween(min, max, currentGuess);
+    setCurrentGuess(newRndNumber);
+  };
+
   return (
     <View style={styles.screen}>
       <Title style={styles.title}>Opponent's Guess</Title>
-      {/* Guess */}
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or Lower?</Text>
+        <View>
+          {/* These two PrimaryButton components call their onPress 
+          function the same way. .bind() must be used to add a function
+          that requires arguments if the function name is only used as 
+          a pointer. */}
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+            -
+          </PrimaryButton>
+          <PrimaryButton
+            onPress={() => {
+              nextGuessHandler("higher");
+            }}
+          >
+            +
+          </PrimaryButton>
+        </View>
       </View>
       <View>{/* Log Rounds */}</View>
     </View>
